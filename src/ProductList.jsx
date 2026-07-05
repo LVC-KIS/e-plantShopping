49909efd918851babe6520a96aa034ab2bc2d8fd
LVC-KIS <elviskisige96@gmail.com>
2026-07-05 07:39:02 -0400

    Final changes

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react';
+import {useDispatch} from 'react-redux';
 import './ProductList.css'
 import CartItem from './CartItem';
+import {addItem} from './CartSlice';
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
     const [addedToCart, setAddedToCart] = useState({});
+    const dispatch = useDispatch();
 
     const plantsArray = [
         {
@@ -255,7 +258,7 @@ function ProductList({ onHomeClick }) {
     };
     const handleAddToCart = (product)=>{
         dispatch(addItem(product));
-        setAddedToCart((prevState) =>({...prevState, [product.name]: true,}));
+        setAddedToCart((prevState) =>({...prevState, [name]:true}));
     };
     return (
         <div>
